3abc12f62a14c2cd768bcbf5b75a5aef13440b31
LokiCoder <lokicoder@gmail.com>
2019-02-03 22:12:39 -0500

    Improve naming in Live2D
    Signed-off-by: LokiCoder <lokicoder@gmail.com>

--- live2d/ui.js
+++ live2d/ui.js
@@ -18,17 +18,17 @@ function getLabel(id, details) {
       child = childs[parts[0]],
       v = parts[1],
       variant = child && child.variants[v]
-  return id + ' ' + (child && (
-    (variant && variant.name)
-    ? variant.name
-    : child.name + ' ' + (v == '00' ? 'Story'
-      : v == '01' ? 'E-A Class'
-      : v == '02' ? 'S Class'
-      : 'Special'
-    )) +
-    ' ' + (details.modder || '') +
-    ' ' + (details.name || '')
-  )
+  return id + ' ' + (child
+    ? ((variant && variant.name)
+      ? variant.name
+      : (child.name || '???') + ' ' + (v == '00' ? 'Story'
+        : v == '01' ? 'E-A Class'
+        : v == '02' ? 'S Class'
+        : 'Special'
+      ))
+    : '???') +
+    (details.modder ? ' ' + details.modder : '') +
+    (details.name ? ' ' + details.name : '')
 }
 function loadData(callback) {
   $.getJSON('./assets.json', function(data) {
