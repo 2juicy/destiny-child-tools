576753673c5b7c5d3df326b8a4f55e0fc1e09862
LokiCoder <lokicoder@gmail.com>
2019-02-04 13:40:41 -0500

    Fix code style
    Signed-off-by: LokiCoder <lokicoder@gmail.com>

--- live2d/ui.js
+++ live2d/ui.js
@@ -28,8 +28,8 @@ function getLabel(id, details) {
       ? variant.name
       : (child.name || '???') + ' ' + (v == '00' ? 'Story'
         : v == '01' ? 'E-A Class'
-        : v == '02' ? 'S Class'
-        : 'Special'
+          : v == '02' ? 'S Class'
+            : 'Special'
       ))
     : '???') +
     (details.modder ? ' ' + details.modder : '') +
@@ -57,19 +57,19 @@ function createComboBox() {
 
     _createAutocomplete: function() {
       var selected = this.element.children(':selected'),
-      value = selected.val() ? selected.text() : ""
+          value = selected.val() ? selected.text() : ''
 
       this.input = $('<input>')
-      .appendTo( this.wrapper )
-      .val( value )
-      .attr('title', '')
-      .addClass('custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left')
-      .autocomplete({
-        delay: 0,
-        minLength: 0,
-        source: $.proxy( this, '_source')
-      })
-      .tooltip({classes: {"ui-tooltip": "ui-state-highlight"}})
+        .appendTo( this.wrapper )
+        .val( value )
+        .attr('title', '')
+        .addClass('custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left')
+        .autocomplete({
+          delay: 0,
+          minLength: 0,
+          source: $.proxy( this, '_source')
+        })
+        .tooltip({classes: {'ui-tooltip': 'ui-state-highlight'}})
 
       this._on( this.input, {
         autocompleteselect: function( event, ui ) {
@@ -78,43 +78,44 @@ function createComboBox() {
             item: ui.item.option
           })
         },
-        autocompletechange: "_removeIfInvalid"
+        autocompletechange: '_removeIfInvalid'
       })
     },
     _createShowAllButton: function() {
       var input = this.input,
-      wasOpen = false
+          wasOpen = false
 
       $('<a>')
-      .attr('tabIndex', -1 )
-      .appendTo( this.wrapper )
-      .button({
-        icons: {
-          primary: "ui-icon-triangle-1-s"
-        },
-        text: false
-      })
-      .removeClass('ui-corner-all')
-      .addClass('custom-combobox-toggle ui-corner-right')
-      .on('mousedown', function() {
-        wasOpen = input.autocomplete('widget').is(':visible')
-      })
-      .on('click', function() {
-        input.trigger('focus')
-        if(wasOpen) return // Close if already visible
-        input.autocomplete('search', '') // Pass empty string as value to search for, displaying all results
-      })
+        .attr('tabIndex', -1 )
+        .appendTo( this.wrapper )
+        .button({
+          icons: {
+            primary: 'ui-icon-triangle-1-s'
+          },
+          text: false
+        })
+        .removeClass('ui-corner-all')
+        .addClass('custom-combobox-toggle ui-corner-right')
+        .on('mousedown', function() {
+          wasOpen = input.autocomplete('widget').is(':visible')
+        })
+        .on('click', function() {
+          input.trigger('focus')
+          if(wasOpen) return // Close if already visible
+          input.autocomplete('search', '') // Pass empty string as value to search for, displaying all results
+        })
     },
 
     _source: function( request, response ) {
       var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), 'i')
       response( this.element.children('option').map(function() {
         var text = $( this ).text()
-        if ( this.value && ( !request.term || matcher.test(text) ) )
-        return {
-          label: text,
-          value: text,
-          option: this
+        if( this.value && ( !request.term || matcher.test(text) ) ) {
+          return {
+            label: text,
+            value: text,
+            option: this
+          }
         }
       }) )
     },
@@ -124,10 +125,10 @@ function createComboBox() {
 
       // Search for a match (case-insensitive)
       var value = this.input.val(),
-      valueLowerCase = value.toLowerCase(),
-      valid = false
+          valueLowerCase = value.toLowerCase(),
+          valid = false
       this.element.children('option').each(function() {
-        if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+        if( $( this ).text().toLowerCase() === valueLowerCase ) {
           this.selected = valid = true
           return false
         }
@@ -137,14 +138,14 @@ function createComboBox() {
 
       // Remove invalid value
       this.input
-      .val('')
-      .attr('title', value + ' didn\'t match any item')
-      .tooltip('open')
+        .val('')
+        .attr('title', value + ' didn\'t match any item')
+        .tooltip('open')
       this.element.val('')
       this._delay(function() {
         this.input.tooltip('close').attr('title', '')
       }, 2500 )
-      this.input.autocomplete('instance').term = ""
+      this.input.autocomplete('instance').term = ''
     },
     _destroy: function() {
       this.wrapper.remove()
