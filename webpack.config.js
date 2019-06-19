8fa80f32c2ae3291c956e071e557e7807a12c65a
LokiCoder <lokicoder@gmail.com>
2019-06-18 20:39:09 -0400

    Create react app skeleton
    Signed-off-by: LokiCoder <lokicoder@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      filename: 'index.html'
+    })
+  ]
+}
